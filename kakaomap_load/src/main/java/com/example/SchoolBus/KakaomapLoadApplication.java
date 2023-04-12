18281309b05f51164af7548970efeb363f1cafe1
조용훈 <97722177+sjsin0905@users.noreply.github.com>
2023-04-12 23:17:23 +0900

    Add files via upload

---
+++ kakaomap_load/src/main/java/com/example/SchoolBus/KakaomapLoadApplication.java
@@ -0,0 +1,14 @@
+package com.example.SchoolBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaomapLoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaomapLoadApplication.class, args);
+	}
+
+
+}
